38a89fc663fd902a6da830976c6afaf09c748ebe
Yue1Ran <s160418002@student.ubaya.ac.id>
2024-04-21 13:31:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a160418002utsanmp/RegistrasiActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a160418002utsanmp
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_registrasi)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
