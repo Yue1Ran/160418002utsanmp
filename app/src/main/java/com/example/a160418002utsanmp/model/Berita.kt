15aa28b4f58962facbff6f639a4fcfe3471c91e4
Yue1Ran <s160418002@student.ubaya.ac.id>
2024-04-21 16:24:04 +0700

    update 3

--- app/src/main/java/com/example/a160418002utsanmp/model/Berita.kt
+++ app/src/main/java/com/example/a160418002utsanmp/model/Berita.kt
@@ -4,8 +4,10 @@ import java.sql.Time
 
 data class Berita (
     val id :String?,
+    val username:String?,
     val image : String?,
     val judul : String?,
+    val des : String?,
     val paragraf : String?,
     val waktu : Time?,
 )
---
+++ app/src/main/java/com/example/a160418002utsanmp/view/BeritaAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.a160418002utsanmp.view
+
+import android.icu.text.NumberFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a160418002utsanmp.R
+import com.example.a160418002utsanmp.model.Berita
+
+class BeritaAdapter (
+    private val beritas: MutableList<Berita> = mutableListOf(),
+    ): RecyclerView.Adapter<BeritaAdapter.ViewHolder>() {
+
+    class ViewHolder(
+        view: View,
+    ) : RecyclerView.ViewHolder(view) {
+        val textjudul: TextView
+        val textUsername: TextView
+        val textDes: TextView
+
+        init {
+            textjudul = view.findViewById(R.id.txtJudulH)
+            textUsername = view.findViewById(R.id.txtUsernameH)
+            textDes = view.findViewById(R.id.txtDesH)
+
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.fragment_berita, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = beritas.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.apply {
+            val berita = beritas[position]
+            textjudul.text =
+               berita.judul
+            textUsername.text =
+                berita.username
+            textDes.text =
+                berita.des
+        }
+    }
+
+    fun updateData(data: List<Berita>) {
+        beritas.clear()
+        beritas.addAll(data)
+        notifyDataSetChanged()
+    }
+}
