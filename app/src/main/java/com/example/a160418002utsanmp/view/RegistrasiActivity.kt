cd54223b4c8c916d082fb68d4d1eb10a8f713013
Yue1Ran <s160418002@gmail.com>
2024-04-21 13:44:04 +0700

    update

--- app/src/main/java/com/example/a160418002utsanmp/view/RegistrasiActivity.kt
+++ app/src/main/java/com/example/a160418002utsanmp/view/RegistrasiActivity.kt
@@ -1,10 +1,9 @@
-package com.example.a160418002utsanmp
+package com.example.a160418002utsanmp.view
 
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.a160418002utsanmp.R
 
 class RegistrasiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
